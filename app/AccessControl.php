62ceb918f88c56cd16b832d258e08e2c9c497d5a
luciobian <lbiancardi91@gmail.com>
2020-03-22 23:50:59 -0300

    feat: Getter para atributo access_timestamp creado.

--- app/AccessControl.php
+++ app/AccessControl.php
@@ -2,9 +2,19 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class AccessControl extends Model
 {
-    //
+    /**
+     * Get the access timestamp.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getAccessTimestampAttribute($value)
+    {
+        return Carbon::parse($value);
+    }
 }
