8785d428ab910573120d5d007687cbb90d5ad460
luciobian <lbiancardi91@gmail.com>
2020-03-23 16:23:59 -0300

    feat: Contenido agregado a la vista home.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,21 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+            <h3>
+                Bienvenido
+                <strong>
+                    {{ Auth::user()->name }}
+                </strong>
+                <hr>
+            </h3>
+            <ul>
+                <li>
+                    Último incio de sesión correcto hace <strong data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="{{$success->access_timestamp}}">{{$success->access_timestamp->diffForHumans()}}</strong>
+                </li>
+                <li>
+                    Último incio de sesión fallido hace <strong  data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="{{$failed->access_timestamp}}">{{$failed->access_timestamp->diffForHumans()}}</strong>
+                </li>
+            </ul>
         </div>
     </div>
 </div>
