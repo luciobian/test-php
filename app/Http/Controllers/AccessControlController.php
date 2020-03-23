66098ae7d388ba157a2cb242989675b81591767a
luciobian <lbiancardi91@gmail.com>
2020-03-23 00:57:51 -0300

    feat: Método show creado.

--- app/Http/Controllers/AccessControlController.php
+++ app/Http/Controllers/AccessControlController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\AccessControl;
 use Illuminate\Http\Request;
 
 class AccessControlController extends Controller
 {
-    //
+    /**
+     * Show view of Access control
+     *
+     * @return void
+     */
+    public function show()
+    {
+        $access_control = AccessControl::orderBy('created_at', 'desc')->get()->groupBy('type');
+
+        return view('access_control', ['access_control'=>$access_control]);
+    }
 }
