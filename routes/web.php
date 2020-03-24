977fb5a67712cf4d80b3ef604eba3122028a6e2e
luciobian <lbiancardi91@gmail.com>
2020-03-23 21:53:51 -0300

    feat: Middleware de autenticación agregado.

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,17 @@ Route::get('/login', function () {
     return view('auth.login');
 });
 
-Route::get('/access-control', 'AccessControlController@show');
+
+Route::group(['middleware'=>'auth'], function(){
+    Route::get('/access-control', 'AccessControlController@show');
+    Route::get('/users', 'UserController@show');
+    Route::delete('/users/{user}', "UserController@destroy")->name('delete');
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/edit/{user}', 'UserController@edit')->name('edit');
+    Route::put('/users/{user}', 'UserController@update')->name('update');
+});
+
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
