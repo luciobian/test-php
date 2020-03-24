7b18c34b2b7092e5eaf38a65d215bd4ef49289d0
luciobian <lbiancardi91@gmail.com>
2020-03-23 23:47:40 -0300

    feat: Labels para table mobile.

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -15,25 +15,25 @@
 
             @foreach ($users as $user)
             <tr>
-                <td class="text-center" style="width:25%">
+                <td data-label="Nombre" class="text-center" style="width:25%">
                     {{ $user->name }}
                 </td>
-                <td class="text-center" style="width:25%">
+                <td data-label="Email" class="text-center" style="width:25%">
                     {{ $user->email }}
                 </td>
-                <td class="text-center" style="width:25%">
+                <td data-label="Fecha de creación" class="text-center" style="width:25%">
                    @if ($user->updated_at == $user->created_at)
                      {{ $user->created_at }}
                    @else
                      {{ $user->updated_at }}  <i class="fa fa-info-circle" aria-hidden="true" href="#" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="Fecha de edición"></i>
                    @endif
                 </td>
-                <td class="text-center" style="width:25%">
+                <td data-label="Operaciones" class="text-center" style="width:25%">
                     <form action="{{ route("delete", ['user'=>$user->id]) }}" method="POST">
                         {{ csrf_field() }}
                         {{ method_field('DELETE') }}
                 
-                        <button href="s" type="submit" class="btn btn-outline-danger" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="Eliminar">
+                        <button href="s" type="submit" class="btn btn-outline-danger" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="Eliminar" {{ $user->id==Auth::user()->id ? 'disabled' : ''}}>
                             <i class="fa fa-trash" aria-hidden="true" ></i>
                         </button>
                         <a href="{{url("/edit/$user->id")}}" type="button" class="btn btn-outline-secondary" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="Modificar">
