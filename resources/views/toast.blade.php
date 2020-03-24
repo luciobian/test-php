a3f3625b902283f04e5d519f6085eb3ed668efb1
luciobian <lbiancardi91@gmail.com>
2020-03-23 23:44:53 -0300

    feat: Vista para mensajes flash creado.

---
+++ resources/views/toast.blade.php
@@ -0,0 +1,31 @@
+@if ($message = Session::get('success'))
+<div class="alert alert-success alert-block container " style="max-width:250px;position: absolute; bottom: 30px; right: 15px;">
+	<button type="button" class="close" data-dismiss="alert">×</button>	
+        <strong>{{ $message }}</strong>
+</div>
+@endif
+
+
+@if ($message = Session::get('error'))
+<div class="alert alert-danger alert-block container "  style="max-width:250px;position: absolute; bottom: 30px; right: 15px;">
+	<button type="button" class="close" data-dismiss="alert">×</button>	
+        <strong>{{ $message }}</strong>
+</div>
+@endif
+
+
+@if ($message = Session::get('warning'))
+<div class="alert alert-warning alert-block container"  style="max-width:250px;position: absolute; bottom: 30px; right: 15px;">
+	<button type="button" class="close" data-dismiss="alert">×</button>	
+	<strong>{{ $message }}</strong>
+</div>
+@endif
+
+
+@if ($message = Session::get('info'))
+<div class="alert alert-info alert-block container"  style="max-width:250px;position: absolute; bottom: 30px; right: 15px;">
+	<button type="button" class="close" data-dismiss="alert">×</button>	
+	<strong>{{ $message }}</strong>
+</div>
+@endif
+    
