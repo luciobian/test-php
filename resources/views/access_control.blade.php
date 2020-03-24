c7ce65b13eeb3d3fc987f37c6af8a4c171d02b12
luciobian <lbiancardi91@gmail.com>
2020-03-23 23:49:25 -0300

    feat: Labels para tablas mobile y totales agregados

--- resources/views/access_control.blade.php
+++ resources/views/access_control.blade.php
@@ -1,21 +1,30 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <h3>Accesos exitosos</h3>
+  <div class="d-flex justify-content-between" >
+    <span><h3>Accesos exitosos</h3></span>
+    <span> <h3>Total: 
+        @if ($success==null)
+            0
+        @else
+          {{ $success->count() }}
+        @endif  
+      </h3></span>
+  </div>
     <hr>
     <table class="table" style="background-color:#ffffff">
         <thead class="thead-light">
           <tr>
-            <th scope="col" class="text-center" style="width:33.3%">Usuario</th>
-            <th scope="col" class="text-center" style="width:33.3%">Fecha y hora</th>
-            <th scope="col" class="text-center" style="width:33.3%">Dirección IP</th>
+            <th  scope="col" class="text-center" style="width:33.3%">Usuario</th>
+            <th  scope="col" class="text-center" style="width:33.3%">Fecha y hora</th>
+            <th  scope="col" class="text-center" style="width:33.3%">Dirección IP</th>
           </tr>
         </thead>
         <tbody>
 
           @foreach ($success as $item)
               <tr>
-                <td class="text-center" style="width:33.3%">
+                <td data-label="Usuario" class="text-center" style="width:33.3%">
                   @if (!empty($item->user))
                     <strong>{{ $item->user->name }}</strong> 
                     ({{$item->user->email}})
@@ -24,12 +33,12 @@
                   @endif
                   
                 </td>
-                <td class="text-center" style="width:33.3%">
+                <td data-label="Fecha y hora" class="text-center" style="width:33.3%">
                   <span  data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="{{$item->access_timestamp->diffForHumans()}}">
                     {{ $item->access_timestamp}}
                   </span>
                 </td>
-                <td class="text-center" style="width:33.3%">{{ $item->ip }}</td>
+                <td data-label="Dirección IP" class="text-center" style="width:33.3%">{{ $item->ip }}</td>
               </tr>
           @endforeach
           
@@ -38,7 +47,16 @@
       <div class="d-flex justify-content-center">
         {{ $success->links() }}
       </div>
-      <h3>Accesos fallidos</h3>
+      <div class="d-flex justify-content-between" >
+        <span><h3>Accesos fallidos</h3></span>
+        <span> <h3>Total: 
+            @if ($failed==null)
+                0
+            @else
+              {{ $failed->count() }}
+            @endif  
+          </h3></span>
+      </div>
     <hr>
     <table class="table" style="background-color:#ffffff">
         <thead class="thead-light">
@@ -51,7 +69,7 @@
         <tbody>
           @foreach ($failed as $item)
               <tr>
-                <td class="text-center" style="width:33.3%">
+                <td data-label="Usuario"  class="text-center" style="width:33.3%">
                   @if (!empty($item->user))
                     <strong>{{ $item->user->name }}</strong> 
                     ({{$item->user->email}})
@@ -59,12 +77,12 @@
                       Usuario desconocido.
                   @endif                  
                 </td>
-                <td class="text-center" style="width:33.3%">
+                <td data-label="Fecha y hora" class="text-center" style="width:33.3%">
                   <span  data-toggle="popover" data-trigger="hover" data-placement="bottom" data-content="{{$item->access_timestamp->diffForHumans()}}">
                     {{ $item->access_timestamp}}
                   </span>
               </td>
-                <td class="text-center" style="width:33.3%">{{ $item->ip }}</td>
+                <td data-label="Dirección IP" class="text-center" style="width:33.3%">{{ $item->ip }}</td>
               </tr>
           @endforeach
         </tbody>
